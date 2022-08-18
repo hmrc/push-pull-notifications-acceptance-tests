8d58574d0d01cd1c81b69391f2d18a044323f8e3
Imran Akram <20515286+mi-akram@users.noreply.github.com>
2022-08-18 16:47:03 +0100

    update box ids

--- src/test/java/definitions/PushPullNotificationsApiDefinitions.java
+++ src/test/java/definitions/PushPullNotificationsApiDefinitions.java
@@ -178,12 +178,12 @@ public class PushPullNotificationsApiDefinitions extends CommonDefinitions {
 
     @When("^I make a request to the validate callback endpoint for a box that does not exist$")
     public void iMakeRequestToTheValidateCallBackEndpointForABoxThatDoesNotExist() {
-        pushPullNotificationsApiSteps.iMakeACallToCallbackWithPayload("3b8e4dd3-a029-4301-a912-1220f3196387", "{\"clientId\" : \"1AgmuykNGEm84u4xMExNKgZB6uqd\", \"callbackUrl\" : \"https://api.isc.qa.tax.service.gov.uk/test/api-platform-test/destination/notifications\"}");
+        pushPullNotificationsApiSteps.iMakeACallToCallbackWithPayload("3b8e4dd3-a029-4301-a912-1220f3196388", "{\"clientId\" : \"1AgmuykNGEm84u4xMExNKgZB6uqd\", \"callbackUrl\" : \"https://api.isc.qa.tax.service.gov.uk/test/api-platform-test/destination/notifications\"}");
     }
 
     @When("^I make a request to the validate callback endpoint with a different client ID$")
     public void iMakeRequestToTheValidateCallBackEndpointWithADifferentClientId() {
-        pushPullNotificationsApiSteps.iMakeACallToCallbackWithPayload("046ceee5-e43f-4159-b5ce-8df5f2b9d1e3", "{\"clientId\" : \"DxSao_5J8fsj3bsgfyr7aWj9UcQa\", \"callbackUrl\" : \"https://api.isc.qa.tax.service.gov.uk/test/api-platform-test/destination/something\"}");
+        pushPullNotificationsApiSteps.iMakeACallToCallbackWithPayload("046ceee5-e43f-4159-b5ce-8df5f2b9d1e3", "{\"clientId\" : \"046ceee5-e43f-4159-b5ce-8df5f2b9d1e3\", \"callbackUrl\" : \"https://api.isc.qa.tax.service.gov.uk/test/api-platform-test/destination/something\"}");
     }
 
     @When("^I make a request to the validate callback endpoint with invalid field names$")
