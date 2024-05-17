7318698f894bc8cec63a822847c8bbc5680f839c
Imran Akram <20515286+mi-akram@users.noreply.github.com>
2024-05-17 15:36:22 +0100

    remove common definitions

--- src/test/java/definitions/apis/CommonResponseDefinitions.java
+++
@@ -1,8 +0,0 @@
-package definitions.apis;
-
-public class CommonResponseDefinitions extends ResponseDefinitions {
-
-//delete this class,
-// 1. rename common response definitions to response definitions,
-// 2. make pushPullNotificationsApiDefinitions extend CommonDefinitions
-}
